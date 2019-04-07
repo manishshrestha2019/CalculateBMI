305a36a433db0d451ba124fb13f974dcc7292408
Softwarica College Lab <softwarica@softwarica.com>
2019-04-07 11:49:20 +0545

    add arthemetic added formula of BMI

--- app/src/main/java/Arthemetic.java
+++ app/src/main/java/Arthemetic.java
@@ -1,2 +1,34 @@
 public class Arthemetic {
+
+
+    float height,weight;
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public Arthemetic(float height, float weight) {
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public float getBIM(){
+        float convertMeter=height/100;
+        float bim=weight/convertMeter*convertMeter;
+        return bim;
+    }
+
+
 }
